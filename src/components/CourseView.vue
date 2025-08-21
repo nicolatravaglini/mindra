9573f21128c293b59e07e7053131871dec8a348e
trava <nicola1.travaglini@gmail.com>
2025-08-21 15:46:22 +0200

    course init

--- src/components/CourseView.vue
+++ src/components/CourseView.vue
@@ -1,29 +1,108 @@
 <script setup>
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useCourseStore } from "../stores/course.js";
 
 // Stores
 const courseStore = useCourseStore();
+
 const progPerc = computed(
     () =>
         ((courseStore.progress.macroIndex + courseStore.progress.microIndex) *
             100) /
         (courseStore.course.length +
-            courseStore.course.reduce((acc, current) => {
-                return acc + current.micro.length;
-            }, 0)),
+            courseStore.course.reduce(
+                (acc, current) => acc + current.micro.length,
+                0,
+            )),
 );
+const visibleMacro = ref({});
+
+function progTest(i) {
+    courseStore.progress.macroIndex = i;
+    courseStore.progress.microIndex = courseStore.course
+        .slice(0, i)
+        .reduce((acc, current) => acc + current.micro.length, 0);
+}
+
+function showMacro(i) {
+    progTest(i);
+    visibleMacro.value[i] = visibleMacro.value[i]
+        ? !visibleMacro.value[i]
+        : true;
+    console.log(visibleMacro.value[i]);
+}
 </script>
 
 <template>
     <div class="container mt-5">
+        <h3>Course - {{ progPerc.toFixed(1) }}%</h3>
+
         <!-- Progress -->
-        <div class="progress">
-            <div class="progress-bar" :style="{ width: progPerc }">
-                {{ progPerc }}%
+        <div class="progress" style="height: 3px">
+            <div
+                class="progress-bar bg-dark"
+                :style="{ width: progPerc + '%' }"
+            ></div>
+        </div>
+
+        <!-- Course -->
+        <div
+            class="mt-4 d-flex flex-column justify-content-start align-items-center gap-3 w-100"
+        >
+            <div
+                v-for="(macro, i) in courseStore.course"
+                :key="i"
+                class="d-flex flex-column justify-content-start align-items-center gap-3 w-100"
+            >
+                <button
+                    class="btn d-flex justify-content-center align-items-center bg-dark text-white text-center shadow w-100 p-5 fs-3"
+                    @click="showMacro(i)"
+                >
+                    {{ macro.title }}
+                </button>
+
+                <div v-if="visibleMacro[i] === true" class="w-100">
+                    <div class="text-dark border rounded-4 p-3">
+                        {{ macro.description }}
+                    </div>
+                    <div
+                        class="d-flex flex-column justify-content-start align-items-start gap-3 mt-3"
+                    >
+                        <div
+                            v-for="(micro, j) in macro.micro"
+                            :key="j"
+                            class="d-flex flex-row justify-content-start align-items-stretch bg-white text-dark text-center text-wrap border rounded-5 shadow-sm p-0 w-100 gap-3"
+                            style="min-height: 150px"
+                        >
+                            <div
+                                class="flex-shrink-0 d-flex justify-content-center align-items-center border-end fs-5 fw-bold p-1"
+                                style="width: 220px; overflow-wrap: break-word"
+                            >
+                                {{ micro.title }}
+                            </div>
+
+                            <div class="flex-grow-1 text-start my-3">
+                                <span class="fw-bold">Description: </span>
+                                {{ micro.description }}
+                            </div>
+
+                            <div
+                                class="flex-shrink-0 d-flex justify-content-center align-items-center"
+                            >
+                                <button
+                                    class="btn bg-dark text-white h-100"
+                                    style="aspect-ratio: 1/1"
+                                >
+                                    <i class="bi bi-play-fill"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
+        <!--
         <div class="accordion mt-3" id="courseAccordion">
             <div
                 class="accordion-item"
@@ -38,6 +117,7 @@ const progPerc = computed(
                         :data-bs-target="`#collapse-${i}`"
                         aria-expanded="false"
                         :aria-controls="`collapse-${i}`"
+                        @click="test(i)"
                     >
                         {{ macro.title }}
                     </button>
@@ -81,6 +161,7 @@ const progPerc = computed(
                 </div>
             </div>
         </div>
+		-->
     </div>
 </template>
 
